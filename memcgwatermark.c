c3c1e8421b6e334c4f8678fd5d279d6265e37a2c
SongJupeng <479158396@qq.com>
2021-08-15 19:07:12 +0800

    memcg watermark code framework

--- memcgwatermark.c
+++ memcgwatermark.c
@@ -1,31 +1,31 @@
 /*
  *  linux/mm/memcontrol.c
  *
- *  此代码框架需要加在memconrol中，增加memory子系统水线分级功能
+ *  代码需要加在memconrol中，增加memory子系统水线分级功能
  *  引入usage high 和low水线，以实现异步回收功能
  *
  */
 
-#include <linux/memcontrol.h>
-#include <linux/page_counter.h>
+//#include <linux/memcontrol.h>
+//#include <linux/page_counter.h>
 
 //水线结构体定义
 //(在 memcontrol.h 中 struct mem_cgroup 里添加)
-#pragma region 
-struct page_counter WMARK_USAGE
+#pragma region
+struct page_counter WMARK_USAGE; //可以直接使用memory.usage_in_bytes
 unsigned long WMARK_LOW;
 unsigned long WMARK_HIGH;
 #pragma endregion
 
 //创建cgroup时，水线初始化，根据cgoup的limit_in_bytes为每个cgoup设定high和low水线
-#pragma region 
-mem_cgroup.WMARK_LOW = 0.6 * limit_in_bytes //启用用户配置时，= low_rate * limit_in_bytes 
-mem_cgroup.WMARK_HIGH = 0.9 * limit_in_bytes //启用用户配置时， = high_rate * limit_in_bytes
+#pragma region
+mem_cgroup.WMARK_LOW = 0.6 * limit_in_bytes; //启用用户配置时，= low_rate * limit_in_bytes
+mem_cgroup.WMARK_HIGH = 0.9 * limit_in_bytes; //启用用户配置时， = high_rate * limit_in_bytes
 //用户自动配置方法，见附件1
 #pragma endregion
 
-//以下代码需要加在每个可能申请内存的函数当中(charge) 
-#pragma region 
+//以下代码需要加在每个可能申请内存的函数当中(charge)
+#pragma region
 /*
 * 函数汇总
 * mem_cgroup_newpage_charge();
@@ -46,17 +46,17 @@ mem_cgroup.WMARK_HIGH = 0.9 * limit_in_bytes //启用用户配置时， = high_r
 	if (memcg_watermarks.WMARK_USAGE > WMARK_HIGH){
         //mem_cgroup结构体中的info成员可以通过node_id和zone_id
         //找到对应的mem_cgroup_per_zone， reclaim not-shared pages first，
-        wake_all_kswapds(order, gfp_mask, ac); 
-        
+        wake_all_kswapds(order, gfp_mask, ac);
+
     }
-		
+
         //注：异步回收功能交给kswapds，它包括如何回收、何时睡眠等
         //    在mm/page_alloc.c可以找到它
 
 #pragma endregion
 
-//以下代码需要加在每个可能释放内存的函数当中(uncharge)
-#pragma region 
+//以下代码需要加在每个可能释放内存的函数当中(uncharge)，以及kswapd循环回收过程中
+#pragma region
 /*
 * 函数汇总
 * mem_cgroup_uncharge_page();
@@ -75,6 +75,5 @@ mem_cgroup.WMARK_HIGH = 0.9 * limit_in_bytes //启用用户配置时， = high_r
 		schedule();
 
 #pragma endregion
-
 
 
